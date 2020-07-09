58c2441dd01379399a91a93777b4a4cd8881bd27
Adrien Cransac <adrien.cransac@gmail.com>
2020-07-09 08:51:08 +0200

    documentation: Reorganized functions and documented them in tester.js

--- tester.js
+++ tester.js
@@ -1,3 +1,11 @@
+// # Test
+
+/*
+ * Make a test. See README
+ * @param {function} testFunction - The test function
+ * @param {string} name - The name of the test. Used in logs
+ * @return {Test}
+ */
 function makeTest(testFunction, name) {
   return [testFunction, name];
 }
@@ -8,6 +16,33 @@ function testFunction(test) {
 
 function name(test) {
   return test[1];
+}
+
+// # Runner
+function checker(testName, fail) {
+  const check = maybeTrue => {
+    if (!maybeTrue) {
+      fail(`Test ${testName} failed!`);
+    }
+    else {
+      return;
+    }
+  };
+
+  return check;
+}
+
+function colourText(text, colour) {
+  switch (colour) {
+    case 'black': return `\u001b[30m${text}\u001b[0m`;
+    case 'red': return `\u001b[31m${text}\u001b[0m`;
+    case 'green': return `\u001b[32m${text}\u001b[0m`;
+    case 'yellow': return `\u001b[33m${text}\u001b[0m`;
+    case 'blue': return `\u001b[34m${text}\u001b[0m`;
+    case 'magenta': return `\u001b[35m${text}\u001b[0m`;
+    case 'cyan': return `\u001b[36m${text}\u001b[0m`;
+    case 'white': return `\u001b[37m${text}\u001b[0m`;
+  }
 }
 
 function reportTestRun(settledTests, testSuiteName) {
@@ -34,6 +69,11 @@ function reportTestRun(settledTests, testSuiteName) {
   return reporter(settledTests, 0, 0);
 }
 
+/*
+ * Run tests, all at once
+ * @param {Test[]} tests - An array of tests
+ * @param {string} [testSuiteName: "Test Suite"] - The name of the test suite. Used in logs
+ */
 function run(tests, testSuiteName) {
   return Promise.allSettled(tests.map(test => new Promise((resolve, reject) => {
     testFunction(test)(() => resolve(), checker(name(test), reject));
@@ -41,6 +81,11 @@ function run(tests, testSuiteName) {
          .then(settledTests => reportTestRun(settledTests, testSuiteName));
 }
 
+/*
+ * Run tests, one after the other
+ * @param {Test[]} tests - An array of tests. They run in the order of this array
+ * @param {string} [testSuiteName: "Test Suite"] - The name of the test suite. Used in logs
+ */
 function runInSequence(tests, testSuiteName) {
   return Promise.allSettled(tests.reduce((previousTests, test) => [...previousTests, new Promise((resolve, reject) => {
     if (previousTests.length === 0) {
@@ -51,32 +96,6 @@ function runInSequence(tests, testSuiteName) {
     }
   })], []))
          .then(settledTests => reportTestRun(settledTests, testSuiteName));
-}
-
-function checker(testName, fail) {
-  const check = maybeTrue => {
-    if (!maybeTrue) {
-      fail(`Test ${testName} failed!`);
-    }
-    else {
-      return;
-    }
-  };
-
-  return check;
-}
-
-function colourText(text, colour) {
-  switch (colour) {
-    case 'black': return `\u001b[30m${text}\u001b[0m`;
-    case 'red': return `\u001b[31m${text}\u001b[0m`;
-    case 'green': return `\u001b[32m${text}\u001b[0m`;
-    case 'yellow': return `\u001b[33m${text}\u001b[0m`;
-    case 'blue': return `\u001b[34m${text}\u001b[0m`;
-    case 'magenta': return `\u001b[35m${text}\u001b[0m`;
-    case 'cyan': return `\u001b[36m${text}\u001b[0m`;
-    case 'white': return `\u001b[37m${text}\u001b[0m`;
-  }
 }
 
 module.exports = { makeTest: makeTest, run: run, runInSequence: runInSequence };
